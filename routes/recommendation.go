f5ac07b6aa800eab99641c5144214bd4bdd7c26c
Kemalcan Bora <kemalcanbora@gmail.com>
2022-02-22 12:20:35 +0300

    about the recommendation

--- routes/recommendation.go
+++ routes/recommendation.go
@@ -1,1 +1,10 @@
 package routers
+
+//==============================================================================================
+// Case: Recommendation
+//==============================================================================================
+// In this case we can use the different models to create a recommendation
+// Probable this way better than a our system;
+// https://www.analyticsvidhya.com/blog/2021/06/build-book-recommendation-system-unsupervised-learning-project/
+
+//==============================================================================================
